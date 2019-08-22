e0b0a63d11de8a9dea5775085c0699ebdbd50e8d
Christopher Tso <christso@gmail.com>
2019-08-22 19:45:29 +1000

    style home page

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,17 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+function trimQuestionText(question) {
+  return '...' + question.optionOne.text.substring(0, 15) + '...';
+}
+
 function Question(props) {
   const { question, author } = props;
   return (
     <div className='card'>
-      <div className='card-heading'>{author.name} asks</div>
-      <br />
-      <img src={author.avatarURL} alt="Avatar" height="100" width="100"></img>
-      <p><strong>Would you rather</strong></p>
-      <p>{question.optionOne.text}</p>
-      <br />
-      <Link to={`/questions/${question.id}`}><button className='button-link'>View Poll</button></Link>
+      <div className='card-heading'>{author.name} asks:</div>
+      <div className='card-content'>
+        <div>
+          <img src={author.avatarURL} alt="Avatar" height="100" width="100"></img>
+        </div>
+        <div className='divider' />
+        <div className='card-info'>        
+          <div style={{fontWeight: 'bold', marginBottom: '15px'}}>Would you rather</div>
+          <div style={{marginBottom: '15px'}}>{trimQuestionText(question)}</div>
+          <Link to={`/questions/${question.id}`}><button className='button-link'>View Poll</button></Link>
+        </div>
+      </div>
     </div>
   )
 }
