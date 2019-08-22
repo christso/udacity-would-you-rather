ae84d7e2bb091ccc19cef5a67994beb7395df2eb
Christopher Tso <christso@gmail.com>
2019-08-22 20:12:18 +1000

    layout QuestionResult

--- src/components/QuestionResult.js
+++ src/components/QuestionResult.js
@@ -7,18 +7,29 @@ function QuestionResult(props) {
   const oneVotes = question.optionOne.votes.length;
   const twoVotes = question.optionTwo.votes.length;
   const totalVotes = oneVotes + twoVotes;
-  
-  console.log(props);
+
   return (
-    <div>
-      <h3>Asked by {author.name}</h3> 
-      <div>{question.optionOne.text} {yourAnswer === OPTION_ONE ? ' (your vote)' : ''}</div>
-      <div>{Math.round(oneVotes/totalVotes*100)}%</div>
-      <div>{oneVotes} out of {totalVotes} votes</div>
-      <br />
-      <div>{question.optionTwo.text} {yourAnswer === OPTION_TWO ? ' (your vote)' : ''}</div>
-      <div>{Math.round(twoVotes/totalVotes*100)}%</div>
-      <div>{twoVotes} out of {totalVotes} votes</div>
+    <div className='card'>
+      <div className='card-heading'>Asked by {author.name}</div>
+      <div className='card-content'>
+        <div>
+          <img src={author.avatarURL} alt="Avatar" height="100" width="100"></img>
+        </div>
+        <div className='divider' />
+        <div>
+          <div style={{fontWeight: 'bold', marginBottom: '10px'}}>Results:</div>
+          <div className='card-stat'>
+            <div>{question.optionOne.text} {yourAnswer === OPTION_ONE ? ' (your vote)' : ''}</div>
+            <div>{Math.round(oneVotes / totalVotes * 100)}%</div>
+            <div>{oneVotes} out of {totalVotes} votes</div>
+          </div>
+          <div className='card-stat'>
+            <div>{question.optionTwo.text} {yourAnswer === OPTION_TWO ? ' (your vote)' : ''}</div>
+            <div>{Math.round(twoVotes / totalVotes * 100)}%</div>
+            <div>{twoVotes} out of {totalVotes} votes</div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
