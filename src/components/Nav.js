c9460bbe44b0e0203f9ee5c6d4c27739ca297aed
Christopher Tso <christso@gmail.com>
2019-08-22 15:26:11 +1000

    add logout button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux';
+import { setAuthedUser } from '../actions/authedUser';
 
-export default function Nav () {
+function Nav(props) {
   return (
-    <nav className='nav'>
-      <ul>
-        <li>
-          <NavLink to='/' exact activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/add' activeClassName='active'>
-            New Question
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/leaderboard' activeClassName='active'>
-            Leader Board
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+      <div className='topnav'>
+        <NavLink to='/' exact activeClassName='active'>
+          Home
+      </NavLink>
+        <NavLink to='/add' activeClassName='active'>
+          New Question
+      </NavLink>
+        <NavLink to='/leaderboard' activeClassName='active'>
+          Leader Board
+      </NavLink>
+      {props.authedUser ? (
+        <div className="topnav topnav-right">
+          <span to='/'>Hello, {props.authedUser.name}</span>
+          <NavLink to='/' onClick={() => props.dispatch(setAuthedUser(null))}>Logout</NavLink>
+        </div>)
+        : null}
+      </div>
   )
 }
+
+function mapStateToProps({ authedUser, users }) {
+  return {
+    authedUser: users[authedUser]
+  };
+}
+
+export default connect(mapStateToProps)(Nav);
